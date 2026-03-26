2ae3e53c86ecdee5e5dba784059aef8b70ec46be
AndersonBaes2004 <andersonbas.39@gmail.com>
2026-03-26 06:57:57 -0600

    Mover SIVWEB a repo principal

---
+++ SIVWEB-Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "./components/login-form"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
